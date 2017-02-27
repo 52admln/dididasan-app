2c3b369b3091ba681ea6a4ee005909e18b2b1afd
52admln <ismrwang@gmail.com>
2017-02-27 09:44:22 +0800

    add 后端上传头像功能

--- README.md
+++ README.md
@@ -11,20 +11,30 @@
 
 ## 遇到问题
 
-问题内容:
-`Uncaught SecurityError:
+1. 问题内容:
+```
+Uncaught SecurityError:
 Failed to execute 'toDataURL' on 'HTMLCanvasElement':
-tainted canvases may not be exported.`
+tainted canvases may not be exported.
+```
 解决方案: http://stackoverflow.com/questions/20424279/canvas-todataurl-securityerror
 
-问题内容: 移动端图片上传裁剪
+2. 问题内容: 移动端图片上传裁剪
 解决方案: https://qiutc.me/post/uploading-image-file-in-mobile-fe.html
 
 
-
+3. 问题内容:
+```
+file_get_contents() expects parameter 1 to be string, array given
+```
 http://stackoverflow.com/questions/14433637/file-get-contents-expects-parameter-1-to-be-string
 
 
+4. 问题内容:上传文件权限问题
+解决方案: http://stackoverflow.com/questions/23540083/failed-to-open-stream-permission-denied-error-laravel
 
-权限问题
-http://stackoverflow.com/questions/23540083/failed-to-open-stream-permission-denied-error-laravel
+5. 问题内容: base64 存为图片无法显示
+解决方案:
+http://bbs.csdn.net/topics/391038262
+http://blog.libnav.com/php/169.html
+
--- api/upload.php
+++ api/upload.php
@@ -14,22 +14,33 @@ header('Content-Type: text/plain; charset=utf-8');
 
 //var_dump($_FILES['file']);
 //echo("===========================");
-$imgData = addslashes (file_get_contents($_FILES['file']['tmp_name']));
+//$imgData = addslashes (file_get_contents($_FILES['file']['tmp_name']));
 //echo(base64_encode($imgData));
 
 
 $fileName = $_FILES['file']['name'];
 $fileType = $_FILES['file']['type'];
 $fileContent = file_get_contents($_FILES['file']['tmp_name']);
-$dataUrl = 'data:' . $fileType . ';base64,' . base64_encode($fileContent);
-$json = json_encode(array(
-    'name' => $fileName,
-    'type' => $fileType,
-    'dataUrl' => $dataUrl
-));
-echo $json;
-$img = base64_encode($fileContent);
-file_put_contents( "img.jpeg", $img);
+$base64_img = base64_encode($fileContent);
+$dataUrl = 'data:' . $fileType . ';base64,' . $base64_img; // 转为 base64 编码
+
+if(file_put_contents( "../uploads/test.jpg", base64_decode($base64_img))) {
+    $json = json_encode(array(
+        'name' => $fileName,
+        'type' => $fileType,
+        'dataUrl' => $dataUrl,
+        'status' => "success"
+    ));
+    echo $json;
+} else {
+    $json = json_encode(array(
+        'name' => $fileName,
+        'type' => $fileType,
+        'dataUrl' => $dataUrl,
+        'status' => "failed"
+    ));
+    echo $json;
+}
 
 // todo  存储文件后将路径存入数据库
 
